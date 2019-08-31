34f11c854005b8e59c00590fe64bafe8127ca838
Guy Lyons <guy@guybar.guybar.org>
2019-08-31 13:50:58 -0400

    update

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -90,20 +90,6 @@
  */
 $databases = [];
 
-
-
-  $databases['migrate']['default'] = array (
-    'database' => 'stats',
-    'username' => 'root',
-    'password' => 'example',
-    'prefix' => '',
-    'host' => 'localhost',
-    'port' => '3306',
-    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-    'driver' => 'mysql',
-  );
-
-
 /**
  * Customizing database settings.
  *
